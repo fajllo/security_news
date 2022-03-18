ce5ba03a5ef327cabae3be2d3b97fb5f7138a025
fsdr <filipsordyl8@gmail.com>
2022-03-18 21:34:02 +0100

    initial set up added tailwind, headless ui and google fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Audiowide&family=Montserrat:wght@100;400;700&display=swap"
+          rel="stylesheet"
+        />
+        <meta property="og:title" content={"cybersec news"} />
+        <meta
+          property="og:description"
+          content={"this a security news website created with next js "}
+        />
+      </Head>
 
-export default MyApp
+      <Component {...pageProps} />
+    </>
+  );
+}
+export default MyApp;
