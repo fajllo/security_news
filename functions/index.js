e923c417e7a54e7166603706658b0309fb5ee0d2
fsdr <filipsordyl8@gmail.com>
2022-03-22 13:00:45 +0100

    darkreading scraping latest news

--- functions/index.js
+++ functions/index.js
@@ -1,71 +1,41 @@
 const functions = require("firebase-functions");
 const cors = require("cors")({ origin: true });
 const puppeteer = require("puppeteer");
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-// async function scraper() {
-//   const browser = await puppeteer.launch({ headless: true });
-//   const page = await browser.newPage();
-
-//   await page.goto("https://www.darkreading.com/");
-//   page.waitFor(5000);
-//   await page.screenshot({ path: "./1.png" });
-//   const data = page.evaluate(() => {
-//     const pTag = document.querySelectorAll("p");
-//     return pTag;
-//   });
-//   await browser.close();
-//   console.log(data);
-//   return data;
-// }
-
-// exports.scrape = functions.https.onRequest((request, response) => {
-//   cors(request, response, async () => {
-//     const data = await scraper();
-
-//     response.send(data);
-//   });
-// });
-const scrapeImages = async () => {
-  //   const browser = await puppeteer.launch({ headless: true });
-  //   const page = await browser.newPage();
-
-  //   await page.goto("https://www.instagram.com/accounts/login/");
-
-  //   // Login form
-
-  //   // Social Page
-
-  //   await page.waitForSelector("img ", {
-  //     visible: true,
-  //   });
-
-  //   // Execute code in the DOM
-  //   const data = await page.evaluate(() => {
-  //     const images = document.querySelectorAll("img");
-
-  //     const urls = Array.from(images).map(v => v.src);
-
-  //     return urls;
-  //   });
-
-  //   await browser.close();
-
-  //   console.log(data);
-
-  return "data";
+const runtimeOpts = {
+  timeoutSeconds: 300,
+  memory: "1GB",
 };
 
-exports.scraper = functions.https.onRequest((request, response) => {
-  cors(request, response, async () => {
-    const data = await scrapeImages();
-    response.send(data);
+async function getNews() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://www.darkreading.com");
+  await page.waitFor(15000);
+  const shortNews = await page.$$eval(".wrap-latest-item", article => {
+    return article.map(news => {
+      let child = {
+        latest: "",
+        summary: "",
+        author: "",
+        img: "",
+        link: "",
+      };
+      child.latest = news.children[0].innerText;
+      child.summary = news.children[1].innerText;
+      child.author = news.children[2].innerText;
+      child.link = news.children[0].children[0].children[0].href;
+      return child;
+    });
   });
-});
+  browser.close();
+  return shortNews;
+}
+
+exports.scraper = functions
+  .runWith(runtimeOpts)
+  .https.onRequest((request, response) => {
+    cors(request, response, async () => {
+      const data = await getNews();
+      response.send(data);
+    });
+  });
