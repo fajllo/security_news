0f965ec5a32986da5751a7b0467a0a145aae10ab
fsdr <filipsordyl8@gmail.com>
2022-03-28 12:19:24 +0200

    adding new news provider

--- components/Providers.js
+++ components/Providers.js
@@ -4,20 +4,28 @@ import Link from "next/link";
 export default function Providers() {
   return (
     <div className="flex flex-col w-screen h-screen justify-center items-center overflow-auto">
-      <Link href="/darkreading">
-        <button
-          className="px-6 h-12 uppercase font-semibold tracking-wider border-2 border-black bg-rose-500 text-black"
-          type="submit"
-        >
-          Dark reading
-        </button>
-      </Link>
       <Link href="/thehackernews">
         <button
           className="px-6 h-12 uppercase font-semibold tracking-wider border-2 border-black bg-sky-500 text-black"
           type="submit"
         >
-          the hacker news
+          The Hacker News
+        </button>
+      </Link>
+      <Link href="/latesthackingnews">
+        <button
+          className="px-6 h-12 uppercase font-semibold tracking-wider border-2 border-black bg-[#eff1f3] text-black"
+          type="submit"
+        >
+          Latest Hacking News
+        </button>
+      </Link>
+      <Link href="/darkreading">
+        <button
+          className="px-6 h-12 uppercase font-semibold tracking-wider border-2 border-black bg-rose-500 text-black"
+          type="submit"
+        >
+          Dark Reading
         </button>
       </Link>
     </div>
--- functions/index.js
+++ functions/index.js
@@ -2,16 +2,22 @@ const functions = require("firebase-functions");
 const cors = require("cors")({ origin: true });
 const puppeteer = require("puppeteer");
 const { initializeApp } = require("firebase-admin/app");
+const admin = require("firebase-admin");
 const { getFirestore } = require("firebase-admin/firestore");
+
 initializeApp();
 const db = getFirestore();
+const dbRef = admin.firestore();
+
 const darkreading = db.collection("darkreading");
+const thehackernews = db.collection("thehackernews");
+const latesthackingnews = db.collection("latesthackingnews");
 const runtimeOpts = {
   timeoutSeconds: 300,
   memory: "1GB",
 };
 
-async function getNews() {
+async function darkReading() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto("https://www.darkreading.com");
@@ -32,19 +38,89 @@ async function getNews() {
       return child;
     });
   });
+
   browser.close();
   for (let news of shortNews) {
     darkreading.add({ ...news });
   }
-
   return shortNews;
 }
 
-exports.scrapy2 = functions
+async function theHackerNews() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://thehackernews.com/");
+
+  const shortNews = await page.$$eval(".story-link", article => {
+    return article.map(news => {
+      let child = {
+        latest: "",
+        summary: "",
+        author: "",
+        link: "",
+      };
+      child.latest = news.children[0].children[1].children[2].innerText;
+      child.summary = news.children[0].children[1].children[0].innerText;
+      child.author = news.children[0].children[1].children[1].innerText;
+      child.link = news.href;
+
+      return child;
+    });
+  });
+  browser.close();
+  for (let news of shortNews) {
+    thehackernews.add({ ...news });
+  }
+  return shortNews;
+}
+async function latestHackingNews() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(
+    "https://latesthackingnews.com/category/cyber-security-news/"
+  );
+
+  const shortNews = await page.$$eval(".list-post", article => {
+    return article.map(news => {
+      let child = {
+        latest: "",
+        summary: "",
+        author: "",
+        link: "",
+      };
+
+      child.summary =
+        news.children[0].children[1].children[0].children[1].innerText;
+      child.latest = news.children[0].children[1].children[1].innerText;
+      child.author = "latesthackingnews.com";
+      child.link =
+        news.children[0].children[1].children[0].children[1].children[0].href;
+
+      return child;
+    });
+  });
+  browser.close();
+  for (let news of shortNews) {
+    latesthackingnews.add({ ...news });
+  }
+  return shortNews;
+}
+async function delData(colName) {
+  const snapDarkReading = await dbRef.collection(colName).get();
+  snapDarkReading.forEach(doc => {
+    dbRef.collection(colName).doc(doc.id).delete();
+  });
+}
+exports.scrapy = functions
   .runWith(runtimeOpts)
   .https.onRequest((request, response) => {
     cors(request, response, async () => {
-      const data = await getNews();
-      response.send(data);
+      delData("darkreading");
+      delData("thehackernews");
+      delData("latesthackingnews");
+      await darkReading();
+      await theHackerNews();
+      await latestHackingNews();
+      response.send("operation complete!");
     });
   });
